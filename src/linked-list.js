eda610319b35bfb8bedff98c1d15b6cae8178743
Alexandr Bet <alex.van.bet@gmail.com>
2019-10-19 23:56:32 +0300

    add task

--- src/linked-list.js
+++ src/linked-list.js
@@ -8,7 +8,7 @@ class LinkedList {
     }
 
     append(data) {
-        const newNode = new Node(data);
+        let newNode = new Node(data);
 
         if (this.length == 0) {
             this._head = newNode
@@ -40,9 +40,33 @@ class LinkedList {
         return actualNode.data;
     }
 
-    insertAt(index, data) {
-
-    }
+    insertAt(index, data) { 
+        let  actualNode = this._head; 
+        let counter = 1;  
+        let newNode = new Node(data); 
+        
+        if (index > this.length){
+            return false
+        }
+        if (index == 0) { 
+            this._head.prev = null; 
+            newNode.next = this._head; 
+            this._head = newNode; 
+        } 
+        else { 
+            while (actualNode) {
+                actualNode =  actualNode.next; 
+                if (counter == index) { 
+                    newNode.prev =  actualNode.prev; 
+                    actualNode.prev.next = newNode; 
+                    newNode.next =  actualNode; 
+                    actualNode.prev = newNode; 
+                    } 
+                counter += 1; 
+            }
+        } 
+    } 
+        
 
     isEmpty() {
         if (this.length == 0){
@@ -59,11 +83,46 @@ class LinkedList {
         this.length = 0;
     }
 
-    deleteAt(index) {}
+    deleteAt(index) {
+        let actualNode = this._head; 
+        let counter = 1;  
+        
+        if (index == 0) { 
+            this._head = this._head.next; 
+            this._head.prev = null; 
+        }
+        else { 
+            while (actualNode) {
+                actualNode =  actualNode.next;    
+                if (actualNode == this._tail) { 
+                    this._tail = this._tail.prev;   
+                    this._tail.next = null;  
+                } 
+                else if (counter == index) { 
+                    actualNode.prev.next = newNode.next; 
+                    actualNode.next.prev = newNode.prex; 
+                } 
+                counter += 1; 
+            }
+        } 
+    }
+
 
     reverse() {}
 
-    indexOf(data) {}
+    indexOf(data) {
+        
+        let actualNode = this._head; 
+        let counter = 0; 
+        while (actualNode) { 
+           if (actualNode.data === data) { 
+               return counter; 
+            } 
+            actualNode = actualNode.next; 
+            counter += 1; 
+       } 
+       return -1; 
+    }
 }
 
 module.exports = LinkedList;
